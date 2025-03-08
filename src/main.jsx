f6faa893bb36ff4b1086c65b52e0e7f1e3f559e1
Luan Ferreira <178341285+luan-42@users.noreply.github.com>
2025-03-08 14:03:57 -0300

    Sincroniza propriedades com o back end
    - Renomeia url_capa para urlCapa
    - Remove Strict Mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
 import './styles/index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <AuthProvider>
         <App />
     </AuthProvider>
-  </StrictMode>
 )
